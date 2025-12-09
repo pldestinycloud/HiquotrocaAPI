169fe514dfd0e2ed7031962d15c2cd30b9568b07
MiguelBarros217 <miguel.barros.96.mb@gmail.com>
2025-12-09 17:02:06 +0000

    Remove ImageDto class from Posts DTOs
    Deleted the ImageDto class from the Posts DTOs. This may be part of a refactor or cleanup to remove unused or redundant DTOs.

--- HiquotrocaAPI/Hiquotroca.API/Domain/Entities/ActionType.cs
+++ HiquotrocaAPI/Hiquotroca.API/Domain/Entities/ActionType.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Hiquotroca.API.Domain.Common;
-using Hiquotroca.API.Domain.Entities.Posts;
+using Hiquotroca.API.Domain.Entities.Post;
 
 namespace Hiquotroca.API.Domain.Entities
 {
